695a9d93394828f890291042bb717b61a755f164
ivi <i.ippolitov@cloud.gcm>
2021-12-29 16:06:18 +0300

    2

--- ucketka_bav/infoinpacts/analyst/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-//получаем корневую директорию сайта
-$array_directory = explode('/', substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']))); 
-
-header("Location:/{$array_directory[1]}/analyst/index.php");
-?>
